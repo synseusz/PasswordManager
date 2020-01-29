fab78916f385d5060c4049f2564f04d64e367069
synseusz <thesynseusz@gmail.com>
2020-01-29 23:45:31 +0000

    Working sql queries & GUI advancements

--- PasswordManager.py
+++ PasswordManager.py
@@ -21,11 +21,12 @@ class PasswordManager:
 
         self.master_pass = Entry(self.root, show = '*', width = 35)
         self.submit_button = Button(self.root, text = "Submit", command = self.submit)
+        self.error_label = Label(self.root, text = "Wrong Password!")
 
         self.Access_check()
 
 # MAIN MENU
-        self.label2 = Label(self.root, text = "Welcome to Password Manager!\n")
+        self.label2 = Label(self.root, text = "Welcome to Password Manager!")
 
         self.store_passw_btn = Button(self.root, text = "Store Password", command = self.store_passw) #change function name
         self.get_passw_btn = Button(self.root, text = "Get Password", command = self.get_passw)
@@ -37,10 +38,21 @@ class PasswordManager:
         except:
             pass
         
-# db functions
+# DB cursor
         self.cursor = self.conn.cursor()
+
+
+# Service & Password entries
+        self.label3 = Label(self.root, text = "What's the name of the service you'd like to store password for?")
         self.service_entry = Entry(self.root)
-        self.test_submit = Button(self.root, text = "insert into db", command = self.add_to_db)
+
+        self.label4 = Label(self.root, text = "Enter password")
+        self.passw_entry = Entry(self.root, show = "*")
+
+        self.store_passw = Button(self.root, text = "Store password", command = self.add_to_db)
+        
+        self.error_label2 = Label(self.root, text = "This service already has a password assigned!")
+
 
 
 
@@ -53,6 +65,7 @@ class PasswordManager:
             try:
                 self.cursor.execute(''' 
                 CREATE TABLE PASSKEYS (
+                    SERVICE TEXT PRIMARY KEY NOT NULL,
                     PASSWD TEXT VARCHAR(100)
                 );
                 ''') 
@@ -63,40 +76,39 @@ class PasswordManager:
             self.Access_forget()
             self.main_menu()
         else:
-            self.label3 = Label(self.root, text = "Wrong Password!")
             try:
-                self.label3.pack()
+                self.error_label.pack()
 
             except:
                 pass
 
-            #self.wp_current_status = self.wrong_passwd[0]
-            #for x in self.wrong_passwd:
-                #if x in self.wp_current_status:
-                    #self.label3.pack()
-                    #print("1 - " + str(self.wrong_passwd))
-                #elif x == 1:
-                    #pass
-                #else:
-                    #print("2 - " + str(self.wrong_passwd))
-                    #pass
-
     def store_passw(self):
-        self.service_entry.pack()
-        self.test_submit.pack()
+        self.store_passwd_menu()
 
 
     def add_to_db(self):
         self.service = self.service_entry.get()
+        self.password = self.passw_entry.get()
 
-        self.command = "INSERT INTO PASSKEYS(PASSWD) VALUES(?);"
-        self.cursor.execute(self.command, [self.service])
+        self.command = "INSERT INTO PASSKEYS(SERVICE,PASSWD) VALUES(?,?);"
+        self.cursor.execute(self.command, [self.service, self.password])
         self.conn.commit()
 
+        # Unique service name check
+        self.query = "SELECT SERVICE FROM PASSKEYS;"
+        self.cursor.execute(self.query)
+
+        self.results = self.cursor.fetchall()
+
+        for x in [self.results]:
+            if x == self.service_entry.get():
+                self.error_label2.pack()
+            else:
+                pass
+
     def get_passw(self):
-        self.cursor = self.conn.cursor()
         self.command2 = "SELECT * FROM 'PASSKEYS';"
-        self.test_get = self.cursor.execute(self.command2)
+        self.cursor.execute(self.command2)
         
         self.results = self.cursor.fetchall()
 
@@ -114,17 +126,32 @@ class PasswordManager:
         self.submit_button.pack(pady = 4)
 
     def main_menu(self):
-        self.label2.pack()
+        self.label2.pack(pady = 10)
         self.store_passw_btn.pack()
         self.get_passw_btn.pack()
         self.exit_btn.pack(pady = 10)
+
+    def store_passwd_menu(self):
+        self.main_menu_forget()
+        self.label3.pack(padx = 10, pady = 10)
+        self.service_entry.pack()
+        self.label4.pack(pady = 6)
+        self.passw_entry.pack()
+        self.store_passw.pack(pady = 5)
 
 
 # 4. REMOVE VIEWS FUNCTIONS
     def Access_forget(self):
         self.label1.pack_forget()
+        self.error_label.pack_forget()
         self.master_pass.pack_forget()
         self.submit_button.pack_forget()
+
+    def main_menu_forget(self):
+        self.label2.pack_forget()
+        self.store_passw_btn.pack_forget()
+        self.get_passw_btn.pack_forget()
+        self.exit_btn.pack_forget()
 
     #######################################################################
     #                             MAIN CONFIG                             #
